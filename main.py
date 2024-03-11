e0a8f581318a26be25d6126f42c2ee8967939781
IgorJovita <igor.jovita@hotmail.com>
2024-03-11 15:10:03 -0300

    Update main.py

--- main.py
+++ main.py
@@ -66,8 +66,7 @@ if st.session_state.botao:
                 if telefone != telefone_coluna:
                     st.write(telefone)
                     st.write(cliente)
-                id_cliente = st.session_state.df_state.loc[
-                    st.session_state.df_state['Nome'] == cliente, 'ID'].to_string(index=False)
+                id_cliente = st.session_state.df_state.loc[cliente, 'ID']
 
                 st.write(id_cliente)
                 st.session_state.lista_pagamento.append((id_cliente, pacote, pagamento, valor))
