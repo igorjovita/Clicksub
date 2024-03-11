cf90583c2b04e15f78708f91793707de75e00e66
IgorJovita <igor.jovita@hotmail.com>
2024-03-11 11:59:39 -0300

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,11 @@ data_reserva = st.date_input('Data da panilha', format='DD/MM/YYYY')
 if st.button('Pesquisar', on_click=pressionar):
     st.session_state.botao = True
 
+state = st.session_state
+
+if 'df_state' not in state:
+    state.df_state = pd.DataFrame(columns=['Nome', 'Telefone', 'Comissario', 'Tipo', 'Fotos'])
+
 if st.session_state.botao:
 
     dados = select_planilha_acqua(data_reserva)
@@ -18,8 +23,9 @@ if st.session_state.botao:
     df = pd.DataFrame(dados, columns=['Nome', 'Telefone', 'Comissario', 'Tipo', 'Fotos'])
 
     df.insert(0, 'Selecionar', [False] * len(df))
+    state.df_state = df
 
-    st.data_editor(df, hide_index=True)
+    st.data_editor(state.df_state, hide_index=True)
 
     if len(st.session_state.df_state.loc[st.session_state.df_state['Selecionar']]) > 0:
         st.write('---')
