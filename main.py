24570a8b7dbc0bb540da89f6467e05d2dcb4f716
IgorJovita <igor.jovita@hotmail.com>
2024-03-11 09:54:13 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 from functions import select_planilha_acqua
 
 st.subheader('Click Sub')
@@ -7,5 +8,8 @@ data_reserva = st.date_input('Data da panilha', format='DD/MM/YYYY')
 
 dados = select_planilha_acqua(data_reserva)
 
+df = pd.DataFrame(dados, columns=['Nome', 'Telefone', 'Comissario', 'Tipo', 'Fotos'])
 
-st.table(dados)
+df.insert(0, 'Selecionar', [False] * len(df))
+
+st.data_editor(df, hide_index=True)
