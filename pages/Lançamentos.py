0c482f8f88c0372e23f1adf7b833faca5d6611f0
IgorJovita <igor.jovita@hotmail.com>
2024-03-20 14:29:16 -0300

    c

---
+++ pages/Lançamentos.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.subheader('Lançamentos')
+
+staffs = ['Diego', 'Cauã', 'Thiago']
+operadoras = ['AcquaWorld', 'Seaquest', 'Pl Divers']
+
+data = st.date_input('Selecione a data', format='DD/MM/YYYY')
+nome_staff = st.selectbox('Selecione o staff', staffs, index=None)
+
+fotos = st.text_input('Quantidade de Fotos')
+video = st.text_input('Quantidade de Videos')
+
+if st.button('Lançar no Sistema'):
+
+    st.write(data, nome_staff, fotos, video)
+
