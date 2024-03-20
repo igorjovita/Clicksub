b0dbdf7909fcd866044d16b4b91819b32b336810
IgorJovita <igor.jovita@hotmail.com>
2024-03-20 14:39:46 -0300

    Update Lançamentos.py

--- pages/Lançamentos.py
+++ pages/Lançamentos.py
@@ -5,12 +5,18 @@ st.subheader('Lançamentos')
 staffs = ['Diego', 'Cauã', 'Thiago']
 operadoras = ['AcquaWorld', 'Seaquest', 'Pl Divers']
 
-data = st.date_input('Selecione a data', format='DD/MM/YYYY')
-nome_staff = st.selectbox('Selecione o staff', staffs, index=None)
-operadora = st.selectbox('Selecione a Operadora', operadoras, index=None)
+col1, col2, col3 = st.columns(3)
 
-fotos = st.text_input('Quantidade de Fotos')
-video = st.text_input('Quantidade de Videos')
+with col1:
+    data = st.date_input('Selecione a data', format='DD/MM/YYYY')
+    fotos = st.text_input('Quantidade de Fotos')
+
+with col2:
+    nome_staff = st.selectbox('Selecione o staff', staffs, index=None)
+    video = st.text_input('Quantidade de Videos')
+
+with col3:
+    operadora = st.selectbox('Selecione a Operadora', operadoras, index=None)
 
 if st.button('Lançar no Sistema'):
 
