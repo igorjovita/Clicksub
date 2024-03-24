91e020f9489ab22359a30be771067cf7560b3cee
IgorJovita <igor.jovita@hotmail.com>
2024-03-24 18:39:21 -0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -37,7 +37,7 @@ def authenticate():
 
     if st.session_state["authentication_status"]:
         with st.sidebar:
-            st.image('logo.png', use_column_width=True)
+
             col1, col2 = st.columns(2)
             with col1:
                 st.subheader(f'*{st.session_state["name"]}*')
